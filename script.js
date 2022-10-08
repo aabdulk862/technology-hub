2bfa25c58d348121a8cb4239c0c8546d01d7c111
Adam Abdulkadir <adama1862@outlook.com>
2022-10-07 21:52:00 -0400

    confetti functionality

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
 // Array of product image names found on the webstie
-const productImages = ["tv1.png", "tv2.png", "tv3.png", "tv4.png", "computer1.png", "computer2.png", "computer3.png", "computer4.png", "phone1.png", "phone2.png", "phone3.png", "phone4.png"];
+const arr = [
+  "tv1.png",
+  "tv2.png",
+  "tv3.png",
+  "tv4.png",
+  "computer1.png",
+  "computer2.png",
+  "computer3.png",
+  "computer4.png",
+  "phone1.png",
+  "phone2.png",
+  "phone3.png",
+  "phone4.png",
+];
+
+const productImages = shuffleArray(arr); // Shuffles product images
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    // Generate random number
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
 
 // Returns Random Integer
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Returns a random product image and loads it on the screen
-// Function is called by the Body element On Load, as well as when the user clicks the Pick a random gift button
-// The function also creates a link that takes the user to the page where the product is located
-function randomGift(){
-    numImages = productImages.length;
-    randomProduct = productImages[getRandomInt(0,numImages-1)]
-    document.getElementById("randomImg").src = "./img/" + randomProduct;
+// Renders a random product image out of an array of product images to the screen
+// Checks to see what kind of image it is, then creates a link that takes the user to the page where the product is located
+// The function is called when the user clicks the "Pick a random gift" button
+// The function is also called by the <body> element in index.html on load
+function randomGift() {
+  numImages = productImages.length;
+  randomProduct = productImages[getRandomInt(0, numImages - 1)];
+  document.getElementById("randomImg").src = "./img/" + randomProduct;
 
-    if(randomProduct.includes("tv")){
-        document.getElementById("randomLink").href = "./tv.html";
-    }
-    if(randomProduct.includes("computer")){
-        document.getElementById("randomLink").href = "./computers.html";
-    }
-    if(randomProduct.includes("phone")){
-        document.getElementById("randomLink").href = "./phones.html";
-    }
+  if (randomProduct.includes("tv")) {
+    document.getElementById("randomLink").href = "./tv.html";
+  }
+  if (randomProduct.includes("computer")) {
+    document.getElementById("randomLink").href = "./computers.html";
+  }
+  if (randomProduct.includes("phone")) {
+    document.getElementById("randomLink").href = "./phones.html";
+  }
 }
