1696c63ed7f95952d5538c62132b24201b5c94d3
Adam Abdulkadir <adama1862@outlook.com>
2022-10-12 17:25:48 -0400

    commenting js

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-// The document's last modification date and time are displayed
+// The Document's last modification Date and Time are displayed
 var myDate = new Date(document.lastModified);
-theMonth = myDate.getMonth() + 1;
-theDate = myDate.getDate();
-theYear = myDate.getFullYear();
-theTime = myDate.toLocaleTimeString();
-document.getElementById("modify").innerHTML =
-  "<I>" +
-  theMonth +
-  "/" +
-  theDate +
-  "/" +
-  theYear +
-  "  at  " +
-  theTime +
-  "</I>";
+var theMonth = myDate.getMonth() + 1;
+var theDate = myDate.getDate();
+var theYear = myDate.getFullYear();
+var theTime = myDate.toLocaleTimeString();
 
-// Displays the Year for Copyright Message
-document.getElementById("year").innerHTML = myDate.getFullYear();
+// Validating that the element exists to avoid errors on the other pages.
+if (document.getElementById("modify") != null) {  
+  // Formatting Date and Time
+  document.getElementById("modify").innerHTML =
+    "<I>" +
+    theMonth +
+    "/" +
+    theDate +
+    "/" +
+    theYear +
+    "  at  " +
+    theTime +
+    "</I>";
+}
+
+// Validating that the element exists
+if (document.getElementById("modify") != null) {  
+  // Displays the Year for Copyright Message
+  document.getElementById("year").innerHTML = myDate.getFullYear();
+}
 
 // Array of product image names found on the webstie
 const arr = [
@@ -46,7 +54,8 @@ function shuffleArray(array) {
   return array;
 }
 
-const productImages = shuffleArray(arr); // Shuffles product images to produce a more random reslut
+// Shuffles product images to produce a more random reslut
+const productImages = shuffleArray(arr); 
 
 // Function Returns Random Integer
 function getRandomInt(min, max) {
@@ -58,24 +67,29 @@ function getRandomInt(min, max) {
 // Renders a random product image out of an array of product images to the screen
 // Checks to see what kind of image it is, then creates a link that takes the user to the page where the product is located
 // The function is called when the user clicks the "Pick a random gift" button
-// The function is also called by the <body> element in index.html on load
+// The function is also called by the window on load
 function randomGift() {
   numImages = productImages.length;
   randomProduct = productImages[getRandomInt(0, numImages - 1)];
-  document.getElementById("randomImg").src = "./img/" + randomProduct;
-
-  if (randomProduct.includes("tv")) {
-    document.getElementById("randomLink").href = "./tv.html";
-  }
-  if (randomProduct.includes("computer")) {
-    document.getElementById("randomLink").href = "./computers.html";
-  }
-  if (randomProduct.includes("phone")) {
-    document.getElementById("randomLink").href = "./phones.html";
+  // Validating that the element exists
+  if (document.getElementById("randomImg") != null) {  
+    document.getElementById("randomImg").src = "./img/" + randomProduct;
+    // Checks to see what type of image it is
+    if (randomProduct.includes("tv")) {
+      document.getElementById("randomLink").href = "./tv.html";
+    }
+    if (randomProduct.includes("computer")) {
+      document.getElementById("randomLink").href = "./computers.html";
+    }
+    if (randomProduct.includes("phone")) {
+      document.getElementById("randomLink").href = "./phones.html";
+    }
   }
 }
 
-
+// Displays the user's name in a welcome message on the homepage
+// The fuction is called when the user presses the enter button
+// The name is saved to local storage
 function setName() {
   userName = document.getElementById("name").value;
   localStorage.setItem("name", userName);
@@ -83,12 +97,18 @@ function setName() {
   document.getElementById("name").value = "";
 }
 
+// Generates Random Gift on Screen
+// If name is found is found in local storage, the name is displayed to the screen
+// If name is not found, the setName function is called
+// The function is called every time the window is loaded
 window.addEventListener("load", (event) => {
-  randomGift()
-  if (localStorage.getItem("name") === null) {
-    setName()
-  }else{
-    document.getElementById("userName").textContent =
-    localStorage.getItem("name");
+  randomGift();
+  if (document.getElementById("username") != null) {  // Validating that the element exists
+    if (localStorage.getItem("name") === null) {
+      setName();
+    } else {
+      document.getElementById("userName").textContent =
+        localStorage.getItem("name");
+    }
   }
 });
