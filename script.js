704bf393ec14ab15d7e050a0facb4eac3d99a479
Adam Abdulkadir <adama1862@outlook.com>
2022-10-29 13:13:54 -0400

    background styles

--- script.js
+++ script.js
@@ -101,6 +101,26 @@ function setName() {
   nameInput.value = "";
 }
 
+function changeBackground(number) {
+  // Chech if background is already showing
+  let prevBG;
+  if(body.className){
+      prevBG = body.className;
+  }
+  // Reset CSS class for body
+  body.className = '';
+switch (number) {
+  case "1":
+      return (prevBG === 'background-1' ? false : body.classList.add('background-1'));
+  case "2":
+      return (prevBG === 'background-2' ? false : body.classList.add('background-2'));
+  case "3":
+      return (prevBG === 'background-3' ? false : body.classList.add('background-3'));
+  default:
+    break;
+}
+}
+
 // Generates Random Gift on Screen
 // If name is found is found in local storage, the name is displayed to the screen
 // If name is not found, the setName function is called
@@ -129,22 +149,3 @@ elToggle.addEventListener("click", function() {
   }
 });
 
-function changeBackground(number) {
-  // Chech if background is already showing
-  let prevBG;
-  if(body.className){
-      prevBG = body.className;
-  }
-  // Reset CSS class for body
-  body.className = '';
-switch (number) {
-  case "1":
-      return (prevBG === 'background-1' ? false : body.classList.add('background-1'));
-  case "2":
-      return (prevBG === 'background-2' ? false : body.classList.add('background-2'));
-  case "3":
-      return (prevBG === 'background-3' ? false : body.classList.add('background-3'));
-  default:
-    break;
-}
-}
