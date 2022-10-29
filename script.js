86f13b6c9f6d0e20dd0fac0fc796103a34e2f311
Adam Abdulkadir <adama1862@outlook.com>
2022-10-28 22:32:15 -0400

    background picker

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const userName = document.getElementById("userName");
 const elToggle  = document.getElementById("toggle");
 const elContent = document.getElementById("elContent");
 let nameInput = document.getElementById("nameInput");
+const { body } = document;
 const arr = [
   "tv1.png",
   "tv2.png",
@@ -127,3 +128,23 @@ elToggle.addEventListener("click", function() {
     elToggle.innerHTML = 'Show';
   }
 });
+
+function changeBackground(number) {
+  // Chech if background is already showing
+  let prevBG;
+  if(body.className){
+      prevBG = body.className;
+  }
+  // Reset CSS class for body
+  body.className = '';
+switch (number) {
+  case "1":
+      return (prevBG === 'background-1' ? false : body.classList.add('background-1'));
+  case "2":
+      return (prevBG === 'background-2' ? false : body.classList.add('background-2'));
+  case "3":
+      return (prevBG === 'background-3' ? false : body.classList.add('background-3'));
+  default:
+    break;
+}
+}
